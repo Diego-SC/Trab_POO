37d950a0543411e36538b0586568f0ebc0f9991a
Ian <133624505+ribeiro-ian@users.noreply.github.com>
2025-01-08 08:34:54 -0300

    Adicionado metodos e atributos da classe Horario

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Horario.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Horario.java
@@ -1,5 +1,46 @@
 package com.mycompany.trab_poo_diego_ian;
+import java.util.HashMap;
 
 public class Horario {
+    private int horaInicio, horaFim, diaDaSemana;
+    private static HashMap <Integer, String> diaSemana = new HashMap<>();;
+    
+    static public void preencheMapDiaSemana(){
+        diaSemana.put(1, "Segunda");
+        diaSemana.put(2, "Terça");
+        diaSemana.put(3, "Quarta");
+        diaSemana.put(4, "Quinta");
+        diaSemana.put(5, "Sexta");
+        diaSemana.put(6, "Sábado");
+        diaSemana.put(7, "Domingo");
+    }
+
+    public Horario(int horaInicio, int horaFim, int diaDaSemana) {
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+        // ajustar horário de fim sempre ser maior que inicio
+        this.diaDaSemana = diaDaSemana;
+        preencheMapDiaSemana();
+    }
+    
+    public int getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(int horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public int getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(int horaFim) {
+        this.horaFim = horaFim;
+    }
+    
+    public int tempoDeAulaMinutos(){
+        return (horaFim - horaInicio)*60; //
+    }
     
 }
