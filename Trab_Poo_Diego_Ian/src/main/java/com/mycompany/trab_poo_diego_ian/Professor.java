d4e47ef566bf16338639cb0a045fd1c66b879126
Ian <133624505+ribeiro-ian@users.noreply.github.com>
2025-01-09 07:49:49 -0300

    Update Professor.java

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Professor.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Professor.java
@@ -1,5 +1,28 @@
 package com.mycompany.trab_poo_diego_ian;
 
-public class Professor {
-    
+public class Professor extends Pessoa{
+    private String siape;
+    private int numeroAulasMinistradas;
+
+    public Professor(String siape, int numeroAulasMinistradas, String c, String n) {
+        super(c, n);
+        this.siape = siape;
+        this.numeroAulasMinistradas = numeroAulasMinistradas;
+    }
+
+    public String getSiape() {
+        return siape;
+    }
+
+    public void setSiape(String siape) {
+        this.siape = siape;
+    }
+
+    public int getNumeroAulasMinistradas() {
+        return numeroAulasMinistradas;
+    }
+
+    public void setNumeroAulasMinistradas(int numeroAulasMinistradas) {
+        this.numeroAulasMinistradas = numeroAulasMinistradas;
+    }
 }
