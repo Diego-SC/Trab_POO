0f9be26dfefe25d241c8801d7fbafe0fbb07fcea
Ian <133624505+ribeiro-ian@users.noreply.github.com>
2025-01-09 07:59:08 -0300

    Adicionado atributos e get/set da classe Sala

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Sala.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Sala.java
@@ -1,5 +1,35 @@
 package com.mycompany.trab_poo_diego_ian;
 
 public class Sala {
-    
+    private int numero,  capacidadeDaSala, numeroDeTurmas;
+
+    public Sala(int numero, int capacidadeDaSala, int numeroDeTurmas) {
+        this.numero = numero;
+        this.capacidadeDaSala = capacidadeDaSala;
+        this.numeroDeTurmas = numeroDeTurmas;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidadeDaSala() {
+        return capacidadeDaSala;
+    }
+
+    public void setCapacidadeDaSala(int capacidadeDaSala) {
+        this.capacidadeDaSala = capacidadeDaSala;
+    }
+
+    public int getNumeroDeTurmas() {
+        return numeroDeTurmas;
+    }
+
+    public void setNumeroDeTurmas(int numeroDeTurmas) {
+        this.numeroDeTurmas = numeroDeTurmas;
+    }
 }
