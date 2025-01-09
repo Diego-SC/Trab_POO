446562f56773892d5391d0e85cec641a53abaffb
Ian <133624505+ribeiro-ian@users.noreply.github.com>
2025-01-09 08:36:11 -0300

    Adicionado escola

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Sala.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Sala.java
@@ -2,11 +2,13 @@ package com.mycompany.trab_poo_diego_ian;
 
 public class Sala {
     private int numero,  capacidadeDaSala, numeroDeTurmas;
+    private Escola escola;
 
-    public Sala(int numero, int capacidadeDaSala, int numeroDeTurmas) {
+    public Sala(int numero, int capacidadeDaSala, int numeroDeTurmas, Escola escola) {
         this.numero = numero;
         this.capacidadeDaSala = capacidadeDaSala;
         this.numeroDeTurmas = numeroDeTurmas;
+        this.escola = escola;
     }
 
     public int getNumero() {
