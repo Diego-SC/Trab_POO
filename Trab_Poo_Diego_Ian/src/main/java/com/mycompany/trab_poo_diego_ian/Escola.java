3c8734c73bc29a3606bd9089fa5d7eb1134b3d56
Ian <133624505+ribeiro-ian@users.noreply.github.com>
2025-01-09 08:05:42 -0300

    Criado atributos da classe Escola

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Escola.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Escola.java
@@ -1,5 +1,29 @@
 package com.mycompany.trab_poo_diego_ian;
 
 public class Escola {
+    String nome, endereco;
+
+    public Escola(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
     
+    // criar metodos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
 }
