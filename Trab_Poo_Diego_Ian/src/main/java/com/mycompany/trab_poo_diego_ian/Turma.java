9f9cbb76495dd8fef5ff5931c13dfc4eec4bf12d
Diego Camillo <71447015+Diego-SC@users.noreply.github.com>
2025-01-09 08:05:18 -0300

    Versão 1.1

--- Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Turma.java
+++ Trab_Poo_Diego_Ian/src/main/java/com/mycompany/trab_poo_diego_ian/Turma.java
@@ -1,5 +1,36 @@
 package com.mycompany.trab_poo_diego_ian;
 
 public class Turma {
+    private String curso;
+    private int anoTurma;
+    private int numeroAlunos;
     
+    Turma(String c, int a, int n){
+        this.curso = c;
+        this.anoTurma = a;
+        this.numeroAlunos = n;
+    }
+    
+    public int TempoAulaSemanalMinutos(){
+        
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public int getAnoTurma() {
+        return anoTurma;
+    }
+    public void setAnoTurma(int anoTurma) {
+        this.anoTurma = anoTurma;
+    }
+    public int getNumeroAlunos() {
+        return numeroAlunos;
+    }
+    public void setNumeroAlunos(int numeroAlunos) {
+        this.numeroAlunos = numeroAlunos;
+    }
 }
